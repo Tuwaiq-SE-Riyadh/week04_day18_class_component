016a0e8390e234c708b737baae9b85ad7a31c856
Faisal-Alsharif <ifaisal.alsharif@gmail.com>
2021-10-27 16:53:54 +0300

    class component lab

---
+++ class-component-lab/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FunctionalHelloWorld from './components/HelloWorld';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionalHelloWorld name="Faisal"/>
+    </div>
+  );
+}
+
+export default App;
